2907f34775ed2e457d6b7d64cc1484a285ac6887
Kristina Rodgers <kristina@roomvaluesteam.com>
2013-07-18 16:42:56 -0500

    [DEV-3582] Upgrade to jetty 9.0.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject info.sunng/ring-jetty9-adapter "0.1.0"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
-  :url "http://github.com/sunng87/ring-jetty9-adapter"
+  :url "http://github.com/getaroom/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -8,6 +8,6 @@
                   :exclusions [javax.servlet/servlet-api]]
                  [ring/ring-servlet "1.2.0-beta2"
                   :exclusions [javax.servlet/servlet-api]]
-                 [org.eclipse.jetty/jetty-server "9.0.2.v20130417"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.0.2.v20130417"]
-                 [org.eclipse.jetty.websocket/websocket-servlet "9.0.2.v20130417"]])
+                 [org.eclipse.jetty/jetty-server "9.0.4.v20130625"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.0.4.v20130625"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.0.4.v20130625"]])
