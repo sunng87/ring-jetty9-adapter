3e5f5b4a6469a3cfb3ef0744827ffe87ef35fa20
Kristina Rodgers <kristina@roomvaluesteam.com>
2013-08-12 09:43:40 -0500

    [DEV-3594] Added configurator back and changed ContextHandlerCollection to the generic HandlerCollection

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject info.sunng/ring-jetty9-adapter "0.1.0"
+(defproject com.getaroom/ring-jetty9-adapter "0.1.0"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
   :url "http://github.com/getaroom/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
@@ -9,5 +9,6 @@
                  [ring/ring-servlet "1.2.0-beta2"
                   :exclusions [javax.servlet/servlet-api]]
                  [org.eclipse.jetty/jetty-server "9.0.4.v20130625"]
+                 [org.eclipse.jetty/jetty-jmx "9.0.4.v20130625"]
                  [org.eclipse.jetty.websocket/websocket-server "9.0.4.v20130625"]
                  [org.eclipse.jetty.websocket/websocket-servlet "9.0.4.v20130625"]])
