ece86af310d59db81b01ba2f29a91ba42aa92e57
Sun Ning <nsun@avos.com>
2013-08-13 10:33:32 +0800

    also update ring libraries

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject info.sunng/ring-jetty9-adapter "0.1.0"
+(defproject info.sunng/ring-jetty9-adapter "0.2.0"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
   :url "http://github.com/getaroom/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.0-beta2"
+                 [ring/ring-core "1.2.0"
                   :exclusions [javax.servlet/servlet-api]]
-                 [ring/ring-servlet "1.2.0-beta2"
+                 [ring/ring-servlet "1.2.0"
                   :exclusions [javax.servlet/servlet-api]]
                  [org.eclipse.jetty/jetty-server "9.0.4.v20130625"]
                  [org.eclipse.jetty/jetty-jmx "9.0.4.v20130625"]
