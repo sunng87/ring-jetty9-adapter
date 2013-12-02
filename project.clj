f6c9e79d8f5d243147ab012e87e6a8d234dfc9ac
Sun Ning <nsun@avos.com>
2013-12-02 16:05:53 +0800

    update dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject info.sunng/ring-jetty9-adapter "0.2.0"
+(defproject info.sunng/ring-jetty9-adapter "0.3.0"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
   :url "http://github.com/getaroom/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.0"
+                 [ring/ring-core "1.2.1"
                   :exclusions [javax.servlet/servlet-api]]
-                 [ring/ring-servlet "1.2.0"
+                 [ring/ring-servlet "1.2.1"
                   :exclusions [javax.servlet/servlet-api]]
-                 [org.eclipse.jetty/jetty-server "9.0.4.v20130625"]
-                 [org.eclipse.jetty/jetty-jmx "9.0.4.v20130625"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.0.4.v20130625"]
-                 [org.eclipse.jetty.websocket/websocket-servlet "9.0.4.v20130625"]])
+                 [org.eclipse.jetty/jetty-server "9.1.0.v20131115"]
+                 [org.eclipse.jetty/jetty-jmx "9.1.0.v20131115"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.1.0.v20131115"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.1.0.v20131115"]])
