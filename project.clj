1645709d45635cfcb17ed6e0c956aaf6f950859f
Sun Ning <sunng@about.me>
2014-07-21 11:48:32 +0800

    update dependencies and version code

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject info.sunng/ring-jetty9-adapter "0.6.2-SNAPSHOT"
+(defproject info.sunng/ring-jetty9-adapter "0.7.0-SNAPSHOT"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
   :url "http://github.com/getaroom/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
                   :exclusions [javax.servlet/servlet-api]]
                  [ring/ring-servlet "1.2.1"
                   :exclusions [javax.servlet/servlet-api]]
-                 [org.eclipse.jetty/jetty-server "9.1.3.v20140225"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.1.3.v20140225"]
-                 [org.eclipse.jetty.websocket/websocket-servlet "9.1.3.v20140225"]]
+                 [org.eclipse.jetty/jetty-server "9.2.1.v20140609"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.2.1.v20140609"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.2.1.v20140609"]]
   :deploy-repositories {"releases" :clojars})
