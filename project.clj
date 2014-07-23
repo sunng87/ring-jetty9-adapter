c7ea8607e4aababe1046b38f939375c4aa9d2c3f
Sun Ning <sunng@about.me>
2014-07-23 15:26:20 +0800

    update ring and clojure.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/getaroom/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.1"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.3.0"
                   :exclusions [javax.servlet/servlet-api]]
-                 [ring/ring-servlet "1.2.1"
+                 [ring/ring-servlet "1.3.0"
                   :exclusions [javax.servlet/servlet-api]]
                  [org.eclipse.jetty/jetty-server "9.2.1.v20140609"]
                  [org.eclipse.jetty.websocket/websocket-server "9.2.1.v20140609"]
