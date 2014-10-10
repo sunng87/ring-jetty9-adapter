dfc3fa9fe69c639228750a3c2692fa69fe723746
Sun Ning <sunng@about.me>
2014-10-10 22:53:23 +0800

    bump dependencies and remove unused ring/ring-core.

--- project.clj
+++ project.clj
@@ -4,11 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.3.0"
+                 [ring/ring-servlet "1.3.1"
                   :exclusions [javax.servlet/servlet-api]]
-                 [ring/ring-servlet "1.3.0"
-                  :exclusions [javax.servlet/servlet-api]]
-                 [org.eclipse.jetty/jetty-server "9.2.1.v20140609"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.2.1.v20140609"]
-                 [org.eclipse.jetty.websocket/websocket-servlet "9.2.1.v20140609"]]
+                 [org.eclipse.jetty/jetty-server "9.2.3.v20140905"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.2.3.v20140905"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.2.3.v20140905"]]
   :deploy-repositories {"releases" :clojars})
