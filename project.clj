e7f5f5694d2b4df5808de47350991b648e5dcd5e
Sun Ning <sunng@about.me>
2014-12-22 10:47:32 +0800

    update jetty to 9.2.6

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-servlet "1.3.2"
                   :exclusions [javax.servlet/servlet-api]]
-                 [org.eclipse.jetty/jetty-server "9.2.3.v20140905"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.2.3.v20140905"]
-                 [org.eclipse.jetty.websocket/websocket-servlet "9.2.3.v20140905"]]
+                 [org.eclipse.jetty/jetty-server "9.2.6.v20141205"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.2.6.v20141205"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.2.6.v20141205"]]
   :deploy-repositories {"releases" :clojars})
