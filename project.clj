5286de6fca03f1351f71424bf9ee3a1a77371c20
Ning Sun <sunng@about.me>
2015-07-08 14:29:28 +0800

    (chore) jetty 9.3

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject info.sunng/ring-jetty9-adapter "0.8.6-SNAPSHOT"
+(def jetty-version "9.3.0.v20150612")
+(defproject info.sunng/ring-jetty9-adapter "0.9.0-SNAPSHOT"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
   :url "http://github.com/getaroom/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-servlet "1.3.2"
+  :dependencies [[ring/ring-servlet "1.4.0-RC2"
                   :exclusions [javax.servlet/servlet-api]]
-                 [org.eclipse.jetty/jetty-server "9.2.11.v20150529"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.2.11.v20150529"]
-                 [org.eclipse.jetty.websocket/websocket-servlet "9.2.11.v20150529"]]
+                 [org.eclipse.jetty/jetty-server ~jetty-version]
+                 [org.eclipse.jetty.websocket/websocket-server ~jetty-version]
+                 [org.eclipse.jetty.websocket/websocket-servlet ~jetty-version]]
   :deploy-repositories {"releases" :clojars}
   :global-vars {*warn-on-reflection* true})
