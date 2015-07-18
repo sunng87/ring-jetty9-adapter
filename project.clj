f5fdfcdff9d53b6cbf9ca12c3cbf5fbd27188286
Ning Sun <sunng@about.me>
2015-07-18 16:22:17 +0800

    (chore) update to latest jetty and ring

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(def jetty-version "9.3.0.v20150612")
+(def jetty-version "9.3.1.v20150714")
 (defproject info.sunng/ring-jetty9-adapter "0.9.0-SNAPSHOT"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
   :url "http://github.com/getaroom/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ring/ring-servlet "1.4.0-RC2"
+  :dependencies [[ring/ring-servlet "1.4.0"
                   :exclusions [javax.servlet/servlet-api]]
                  [org.eclipse.jetty/jetty-server ~jetty-version]
                  [org.eclipse.jetty.websocket/websocket-server ~jetty-version]
