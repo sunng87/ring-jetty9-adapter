5977ffaea75b54e13965f7a2890917193d438eaa
Ning Sun <sunng@protonmail.com>
2018-09-01 08:45:50 +0800

    Version 0.11.4

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def jetty-version "9.4.12.v20180830")
-(defproject info.sunng/ring-jetty9-adapter "0.11.4-SNAPSHOT"
+(defproject info.sunng/ring-jetty9-adapter "0.11.4"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
   :url "http://github.com/sunng87/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
