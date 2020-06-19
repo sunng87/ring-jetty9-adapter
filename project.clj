318eaecab7dc3246e732a1ed95e1b26a0b8d93d0
Ning Sun <sunng@protonmail.com>
2020-06-19 09:23:46 +0800

    (feat) update jetty

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def jetty-version "9.4.28.v20200408")
+(def jetty-version "9.4.30.v20200611")
 
 (defproject info.sunng/ring-jetty9-adapter "0.12.9-SNAPSHOT"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
