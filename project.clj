90a76a105d2ff15860e22f7d1392cd23c569cca2
Ning Sun <sunng@protonmail.com>
2020-07-29 22:22:55 +0800

    (fix) cleanup ssl providers and drop conscrypt support

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def jetty-version "9.4.30.v20200611")
 
-(defproject info.sunng/ring-jetty9-adapter "0.13.1-SNAPSHOT"
+(defproject info.sunng/ring-jetty9-adapter "0.14.0-SNAPSHOT"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
   :url "http://github.com/sunng87/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
@@ -12,9 +12,7 @@
                  [org.eclipse.jetty.websocket/websocket-server ~jetty-version]
                  [org.eclipse.jetty.websocket/websocket-servlet ~jetty-version]
                  [org.eclipse.jetty.http2/http2-server ~jetty-version]
-                 [org.eclipse.jetty/jetty-alpn-server ~jetty-version]
-                 [org.eclipse.jetty/jetty-alpn-conscrypt-server ~jetty-version :exclusions [[org.conscrypt/conscrypt-openjdk-uber]]]
-                 [org.conscrypt/conscrypt-openjdk "2.4.0" :classifier "linux-x86_64"]]
+                 [org.eclipse.jetty/jetty-alpn-server ~jetty-version]]
   :deploy-repositories {"releases" :clojars}
   :global-vars {*warn-on-reflection* true}
   :jvm-args ["-Xmx128m"]
@@ -23,34 +21,11 @@
              ;; for openjdk8
              :example-http2-openjdk8 {:source-paths ["examples/"]
                                       :main ^:skip-aot rj9a.http2
-                                      :dependencies ^:replace [[org.clojure/clojure "1.10.1"]
-                                                               [ring/ring-servlet "1.8.1"
-                                                                :exclusions [javax.servlet/servlet-api]]
-                                                               [org.eclipse.jetty/jetty-server ~jetty-version]
-                                                               [org.eclipse.jetty.websocket/websocket-server ~jetty-version]
-                                                               [org.eclipse.jetty.websocket/websocket-servlet ~jetty-version]
-                                                               [org.eclipse.jetty.http2/http2-server ~jetty-version]
-                                                               ;; OpenJDK8 ALPN
-                                                               [org.eclipse.jetty.alpn/alpn-api "1.1.3.v20160715"]
-                                                               [org.eclipse.jetty/jetty-alpn-server ~jetty-version]
-                                                               [org.eclipse.jetty/jetty-alpn-openjdk8-server ~jetty-version]]
-                                      :boot-dependencies [[org.mortbay.jetty.alpn/alpn-boot "8.1.12.v20180117"
-                                                           :prepend true]]
-                                      :plugins [[info.sunng/lein-bootclasspath-deps "0.3.0"]]}
+                                      :dependencies [[org.eclipse.jetty/jetty-alpn-openjdk8-server ~jetty-version]]}
              ;; for jdk9+
-             :example-http2-jdk9 {:source-paths ["examples/"]
+             :example-http2-jdk11 {:source-paths ["examples/"]
                                   :main ^:skip-aot rj9a.http2
-                                  :dependencies ^:replace [[org.clojure/clojure "1.10.1"]
-                                                           [ring/ring-servlet "1.8.1"
-                                                            :exclusions [javax.servlet/servlet-api]]
-                                                           [org.eclipse.jetty/jetty-server ~jetty-version]
-                                                           [org.eclipse.jetty.websocket/websocket-server ~jetty-version]
-                                                           [org.eclipse.jetty.websocket/websocket-servlet ~jetty-version]
-                                                           [org.eclipse.jetty.http2/http2-server ~jetty-version]
-                                                           ;; JDK9 ALPN
-                                                           [org.eclipse.jetty.alpn/alpn-api "1.1.3.v20160715"]
-                                                           [org.eclipse.jetty/jetty-alpn-server ~jetty-version]
-                                                           [org.eclipse.jetty/jetty-alpn-java-server ~jetty-version]]}
+                                  :dependencies [[org.eclipse.jetty/jetty-alpn-java-server ~jetty-version]]}
              :example-websocket {:source-paths ["examples/"]
                                  :main ^:skip-aot rj9a.websocket}
              :example-http {:source-paths ["examples/"]
