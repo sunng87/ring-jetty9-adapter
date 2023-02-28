f061225e11c8b11a6c3d4fb64e0f5459ff093652
Ning Sun <sunng@protonmail.com>
2023-02-28 10:22:19 +0800

    feat: update jetty to 11.0.14

--- http3/project.clj
+++ http3/project.clj
@@ -1,6 +1,6 @@
-(def jetty-version "11.0.13")
+(def jetty-version "11.0.14")
 
-(defproject info.sunng/ring-jetty9-adapter-http3 "0.2.1"
+(defproject info.sunng/ring-jetty9-adapter-http3 "0.2.2"
   :description "Ring adapter for jetty 9 and above, meta package for http3"
   :url "http://github.com/sunng87/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
