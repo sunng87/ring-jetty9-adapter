999e95c6cb5f03d6a05bc799cb12b17c997be616
Ning Sun <sunng@protonmail.com>
2023-12-10 20:20:52 +0800

    chore: bump version

--- http3/project.clj
+++ http3/project.clj
@@ -1,6 +1,6 @@
 (def jetty-version "12.0.4")
 
-(defproject info.sunng/ring-jetty9-adapter-http3 "0.4.2"
+(defproject info.sunng/ring-jetty9-adapter-http3 "0.4.3"
   :description "Ring adapter for jetty 9 and above, meta package for http3"
   :url "http://github.com/sunng87/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
