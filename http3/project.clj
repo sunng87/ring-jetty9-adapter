39430ec58db586c15730081907936d4ae0ae6b71
Ning Sun <sunng@protonmail.com>
2024-03-09 09:42:08 -0800

    chore: bump version for http3 module

--- http3/project.clj
+++ http3/project.clj
@@ -1,6 +1,6 @@
 (def jetty-version "12.0.7")
 
-(defproject info.sunng/ring-jetty9-adapter-http3 "0.5.0-SNAPSHOT"
+(defproject info.sunng/ring-jetty9-adapter-http3 "0.5.0"
   :description "Ring adapter for jetty 9 and above, meta package for http3"
   :url "http://github.com/sunng87/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
