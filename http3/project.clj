5bb5667dc9dc0a3ab336ec67311308778008c8de
Ning Sun <sunng@protonmail.com>
2025-05-08 20:53:04 +0800

    feat: jetty 12.0.20

--- http3/project.clj
+++ http3/project.clj
@@ -1,6 +1,6 @@
-(def jetty-version "12.0.19")
+(def jetty-version "12.0.20")
 
-(defproject info.sunng/ring-jetty9-adapter-http3 "0.6.6"
+(defproject info.sunng/ring-jetty9-adapter-http3 "0.6.7"
   :description "Ring adapter for jetty 9 and above, meta package for http3"
   :url "http://github.com/sunng87/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def jetty-version "12.0.19")
+(def jetty-version "12.0.20")
 
 (defproject info.sunng/ring-jetty9-adapter "0.37.4-SNAPSHOT"
   :description "Ring adapter for jetty9, which supports websocket and spdy"
@@ -8,8 +8,8 @@
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.ring-clojure/ring-core-protocols "1.14.1"]
                  [org.ring-clojure/ring-websocket-protocols "1.14.1"]
-                 [info.sunng/ring-jetty9-adapter-http2 "0.1.6" :optional true]
-                 [info.sunng/ring-jetty9-adapter-http3 "0.6.6" :optional true]
+                 [info.sunng/ring-jetty9-adapter-http2 "0.1.7" :optional true]
+                 [info.sunng/ring-jetty9-adapter-http3 "0.6.7" :optional true]
                  [org.eclipse.jetty/jetty-server ~jetty-version]
                  [org.eclipse.jetty/jetty-util ~jetty-version]
                  [org.eclipse.jetty.websocket/jetty-websocket-jetty-api ~jetty-version]
@@ -34,7 +34,7 @@
                              :main ^:skip-aot rj9a.http3}
              :example-websocket {:source-paths ["examples/"]
                                  :dependencies [[org.eclipse.jetty/jetty-slf4j-impl ~jetty-version]
-                                                [ring/ring-core "1.12.1"]]
+                                                [ring/ring-core "1.14.1"]]
                                  :main ^:skip-aot rj9a.websocket}
              :example-http {:source-paths ["examples/"]
                             :dependencies [[org.eclipse.jetty/jetty-slf4j-impl ~jetty-version]]
