c077a82961c298f96ff17425bcbbfecf4a052bf7
Ning Sun <sunng@protonmail.com>
2025-08-23 18:45:09 +0800

    fix: configuration change for http3 api

--- http3/src/ring/adapter/jetty9/http3.clj
+++ http3/src/ring/adapter/jetty9/http3.clj
@@ -12,7 +12,7 @@
                  (let [camel-case-item (clojure.string/replace (name item) #"-." #(clojure.string/upper-case (subs % 1)))
                        pascal-case-item (str (clojure.string/upper-case (subs camel-case-item 0 1)) (subs camel-case-item 1))]
                    [`(contains? ~options ~(keyword item))
-                    `(doto (. ~(symbol (str "set" pascal-case-item)) ~(keyword item)))]))
+                    `(doto (. ~(symbol (str "set" pascal-case-item)) (~(keyword item) ~options)))]))
                config-items)))
 
 (defn- default-sesison-listener []
@@ -20,24 +20,16 @@
 
 (defn- http3-server-connection-factory
   "Configure http3 specific options on HTTP3ServerConnectionFactory"
-  [quic-config http3-options]
-  (let [http3-config-factory
-        (RawHTTP3ServerConnectionFactory. quic-config (default-sesison-listener))
+  []
+  ;;TODO: http config
+  (let [http3-connection-factory
+        (RawHTTP3ServerConnectionFactory. (default-sesison-listener))]
 
-        ^HTTP3Configuration
-        http3-configuration (.getHTTP3Configuration http3-config-factory)]
-
-    (cond->-config-options http3-configuration http3-options
-                           [input-buffer-size max-blocked-streams
-                            max-request-headers-size max-response-headers-size
-                            output-buffer-size stream-idle-timeout
-                            use-input-direct-byte-buffers use-output-direct-byte-buffers])
-
-    http3-config-factory))
+    http3-connection-factory))
 
 (defn- server-quic-configuration
   "Configure quic specific options on HTTP3ServerConnector"
-  [ssl-context-factory pem-work-directory quic-options]
+  [pem-work-directory quic-options]
   (let [^QuicheServerQuicConfiguration quic-config
         (QuicheServerQuicConfiguration. (Path/of pem-work-directory (into-array String [])))]
 
@@ -56,7 +48,7 @@
 
 (defn quic-server-connector [server http3-options ssl-context-factory pem-work-directory port host]
   (let [quic-config (server-quic-configuration pem-work-directory http3-options)
-        connection-factory (http3-server-connection-factory quic-config http3-options)
+        connection-factory (http3-server-connection-factory)
         connector (QuicheServerConnector. server ssl-context-factory ^QuicheServerQuicConfiguration quic-config
                                           (into-array RawHTTP3ServerConnectionFactory [connection-factory]))]
     (doto connector
