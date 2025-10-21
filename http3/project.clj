698c66281f77c9ab4382630a1008b3e138c3097f
Ning Sun <sunng@protonmail.com>
2025-10-22 03:43:02 +0800

    feat: jetty 12.1.3, ring 1.15 (#165)

--- http3/project.clj
+++ http3/project.clj
@@ -1,12 +1,12 @@
-(def jetty-version "12.1.1")
+(def jetty-version "12.1.3")
 
-(defproject info.sunng/ring-jetty9-adapter-http3 "0.7.1"
+(defproject info.sunng/ring-jetty9-adapter-http3 "0.7.2"
   :description "Ring adapter for jetty 9 and above, meta package for http3"
   :url "http://github.com/sunng87/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories {"releases" :clojars}
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure "1.12.2"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.eclipse.jetty.http3/jetty-http3-server ~jetty-version]
                  [org.eclipse.jetty.quic/jetty-quic-quiche-server ~jetty-version]])
